237a554f367600fc3bf073086b2e3a445bb84e1e
Adam Sarli <adam.sarli@gmail.com>
2020-01-04 12:21:22 -0500

    added github and demo video links to data and display

--- src/components/projects/Project.js
+++ src/components/projects/Project.js
@@ -4,7 +4,19 @@ const Project = ({project}) => {
   return (
     <div className="project">
       <h2>{project.title}</h2>
+      <iframe
+        title="video preview"
+        id="video" type="text/html"
+        width="360" height="220"
+        src={project.demoUrl}
+        frameBorder="0"
+        allowFullScreen
+        >
+      </iframe>
       <p>{project.description}</p>
+      <a href={project.githubUrl} target="_blank">
+        <img className="github-logo" src="/images/github.png" alt="github icon"/>
+      </a>
     </div>
   )
 }
--- src/components/projects/projectData.js
+++ src/components/projects/projectData.js
@@ -1,14 +1,20 @@
 export const myProjects = [
   {
     title: "Student Agendas",
-    description: "an app for teachers to create video agendas!"
+    description: "an app for teachers to create video agendas!",
+    githubUrl: "https://github.com/mathycoder/student-agendas-fullstack",
+    demoUrl: "http://www.youtube.com/embed/VXla-HpaPUI"
   },
   {
     title: "MyGradebook",
-    description: "A standards-based gradebook!"
+    description: "A standards-based gradebook!",
+    githubUrl: "https://github.com/mathycoder/mygradebook-js",
+    demoUrl: "http://www.youtube.com/embed/6h50pqRM3aI"
   },
   {
-    title: "Memory Lane",
-    description: "an app about memories!"
+    title: "MemoryLane",
+    description: "an app about memories!",
+    githubUrl: "https://github.com/mathycoder/memory_lane",
+    demoUrl: "http://www.youtube.com/embed/d3hfnxCV2V4"
   }
 ]
